7582518475d7484cb7c6853b0fc07385dde8c7fb
uPadrin <bryan.vieira2013@gmail.com>
2024-01-16 09:03:42 -0300

    feat: init react

---
+++ src/assets/pages/home/Home.tsx
@@ -0,0 +1,43 @@
+function Home() {
+    return (
+        <>
+            <div
+                style={{
+                    width: "100vw",
+                    display: "flex",
+                    justifyContent: "center",
+                }}
+            >
+                <div>
+                    <div
+                        style={{
+                            width: "80vw",
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                        }}
+                    >
+                        <h2>Seja Bem vindo! </h2>
+                        <p>Expresse aqui os seus pensamentos e opiniões</p>
+                    </div>
+                    <div
+                        style={{
+                            width: "80vw",
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                        }}
+                    >
+                        <img
+                            src="https://i.imgur.com/VpwApCU.png"
+                            alt="Imagem da Página Home"
+                            width="400px"
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Home;
